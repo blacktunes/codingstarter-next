e8be177167d38a654ab8e5a8e07481b37556ab0a
blacktunes <blacktunes@foxmail.com>
2021-08-21 20:28:25 +0800

    feat: add github link

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,9 @@ export default function Header() {
             CodingStarter
           </a>
         </Link>
+        <span className="block sm:inline text-sm ml-0 mt-2 sm:mt-0 sm:ml-6 font-normal text-gray-400 dark:text-gray-500">
+          <a href="https://github.com/blacktunes/codingstarter-next" target="_blank" className="text-purple-500 dark:text-purple-600 border-b-2 border-purple-800 border-opacity-20 dark:border-opacity-80" rel="noreferrer">GitHub Repository</a>
+        </span>
       </h1>
     </div>
   )
